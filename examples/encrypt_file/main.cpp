032499a5a0fd68a2abd93bda51508fc4f313fd71
Ferenc Nagy <jester.nf@gmail.com>
2010-08-15 16:59:00 +0200

    Improved the 'encrypt_file' example with command line arguments\n

--- examples/encrypt_file/main.cpp
+++ examples/encrypt_file/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <fstream>
 #include <algorithm>
 #include <iterator>
@@ -8,17 +9,40 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-    cipher::CaesarCipher caesar(5);
+void print_usage();
 
-    ifstream input("lyrics.txt");
-    ofstream output("lyrics_encrypted.txt");
+int main(int argc, char** argv) {
+    if(argc < 5) {
+	print_usage();
+	return -1;
+    }
+    enum Mode{ENCRYPT, DECRYPT};
+    Mode m;
+    if(!strcmp(argv[3], "encrypt")) {
+	m = ENCRYPT;
+    } else if(!strcmp(argv[3], "decrypt")){
+	m = DECRYPT;
+    } else {
+	print_usage();
+	return -1;
+    }
+    string ifName(argv[1]);
+    string ofName(argv[2]);
+    const int shift = atoi(argv[4]);
+    cipher::CaesarCipher caesar(shift);
+
+    ifstream input(ifName.c_str());
+    ofstream output(ofName.c_str());
     if(!input.good() || !output.good()) {
         cerr << "Input or output file cannot be opened! Terminating..." << endl;
         return -1;
     }
 
-    cipher::FunctorObject* functor = new cipher::DecryptFunctor(&caesar);
+    cipher::FunctorObject* functor;
+    m == ENCRYPT ? 
+		functor = new cipher::EncryptFunctor(&caesar) :
+		functor = new cipher::DecryptFunctor(&caesar);
+		    
     cipher::FunctorWrapper wrapper(functor);
     transform(istream_iterator<string>(input), istream_iterator<string>(), ostream_iterator<string>(output), wrapper);
     delete functor;
@@ -26,4 +50,8 @@ int main(int argc, char** argv) {
     wrapper.setFunctorObject(0);
 
     return 0;
+}
+
+void print_usage() {
+    cout << "Usage: main <input_file> <output_file> <encrypt/decrypt> <shift>" << endl;
 }
